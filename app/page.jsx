dc427faa1d0cac5754874a711cf3c84d89e01058
Anjali Arya <anjalisinha498@gmail.com>
2023-05-10 23:58:31 +0530

    added google authentication

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,23 @@
- import React from 'react';
  
- const Page = () => {
+ const Home = () => {
     return (
         <div>
-            
+            <section className="w-full flex justify-center items-center flex-col">
+                <h1 className="head_text text-center">
+                    Discover & Share
+                    
+                    <br className="max-md:hidden" />
+                        
+                    <span className="orange_gradient text-center "> AI-Powered Prompts </span>
+                </h1>
+
+                <p className="desc text-center">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad soluta tempore aut repellendus quibusdam velit, officiis quo eos enim ut quae dolorem minus accusamus nihil delectus dolorum magni consequatur. Quo.
+                </p>
+            </section>
         </div>
     );
  }
  
- export default Page;
+ export default Home;
  
